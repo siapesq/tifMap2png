107ab6537a04069cab904fc0e95de8b9a9bccbc8
36thChamber <arturcf00@gmail.com>
2024-03-21 09:19:32 -0300

    Update image_converter.py
    Revertendo para folder local da ec2

--- image_converter.py
+++ image_converter.py
@@ -1,46 +1,18 @@
-import boto3
-import os
 from image import process_image
 
-# Define as credenciais da AWS
-aws_access_key_id = ''
-aws_secret_access_key = ''
+# Define o caminho para a pasta 'dashboard'
+input_folder = "/home/ubuntu/tif/"
 
-# Cria uma sessão com as credenciais especificadas
-session = boto3.session.Session(
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
-)
+# Obtém todos os arquivos TIFF dentro da pasta 'dashboard'
+input_files = glob(os.path.join(input_folder, '*.tif'))
 
-# Cria um cliente S3 com a sessão configurada
-s3_client = session.client('s3')
-
-# Define o nome do bucket da AWS
-bucket_name = 'tifs-siapesq'
-
-# Lista todos os objetos no bucket
-response = s3_client.list_objects_v2(Bucket=bucket_name)
-
-# Obtém todos os objetos com a extensão '.tif'
-input_files = [obj['Key'] for obj in response.get('Contents', []) if obj['Key'].endswith('.tif')]
-
-# Define o caminho para a pasta onde os arquivos PNG serão salvos
-output_folder = "/home/ubuntu/png"
-
-# Cria o diretório de saída se ainda não existir
-os.makedirs(output_folder, exist_ok=True)
+output_folder = "/home/ubuntu/map-api/png"
 
 for input_file in input_files:
     try:
-        # Define o caminho completo do arquivo de saída
         output_file = os.path.join(output_folder, os.path.basename(input_file).replace('.tif', '.png'))
-        
-        # Faz o download do arquivo TIFF do bucket
-        s3_client.download_file(bucket_name, input_file, output_file)
-        
-        # Processa a imagem - agora usando o caminho local do arquivo baixado
-        process_image(output_file)  # Ajuste aqui para usar output_file
-        
+        process_image(input_file, output_file)
     except Exception as e:
         print(f"Erro ao processar a imagem {input_file}: {e}")
+
 
